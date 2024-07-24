d7ebe00e74578f3eb7dfeb96cefcffa1a552e9ac
Andrew Kodkod <678665+akodkod@users.noreply.github.com>
2024-07-24 12:25:58 +0700

    Switch to TypeScript

--- bun.config.ts
+++ bun.config.ts
@@ -1,22 +1,23 @@
 import path from 'path';
 import fs from 'fs';
+import type { BuildConfig } from 'bun';
 
-const config = {
+const config: BuildConfig = {
   sourcemap: "external",
   entrypoints: ["app/javascript/application.js"],
   outdir: path.join(process.cwd(), "app/assets/builds"),
 };
 
-const build = async (config) => {
+const build = async (config: BuildConfig) => {
   const result = await Bun.build(config);
 
   if (!result.success) {
     if (process.argv.includes('--watch')) {
       console.error("Build failed");
+
       for (const message of result.logs) {
         console.error(message);
       }
-      return;
     } else {
       throw new AggregateError(result.logs, "Build failed");
     }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,10 @@
-module.exports = {
+import type { Config } from "tailwindcss"
+
+export default {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ]
-}
+} satisfies Config
