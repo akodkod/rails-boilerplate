08c76ad89ecb347d755540aefdc7e3da9ae981ee
Andrew Kodkod <678665+akodkod@users.noreply.github.com>
2024-08-01 13:04:38 +0700

    Improve vite DX

--- app/javascript/entrypoints/application.ts
+++ app/javascript/entrypoints/application.ts
@@ -1,4 +1,12 @@
-// Entry point for the build script in your package.json
+import { Application } from "@hotwired/stimulus"
+import { registerControllers } from 'stimulus-vite-helpers'
 import "@hotwired/turbo-rails"
-import "../controllers"
 import "../styles/app.css"
+
+const application = Application.start()
+application.debug = false
+window.Stimulus = application
+
+// Load Stimulus controllers
+const controllers = import.meta.glob("~/**/*_controller.ts", { eager: true })
+registerControllers(application, controllers)
